bd7e85e747cd69e3fb6a7f5b6c505ad97999f790
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-07-20 15:17:29 -0300

    text-shadow

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -11,13 +11,11 @@ export const Logo = styled.Image`
   margin: 126px 241px 122px;
 `;
 
-export const SubmitButton = styled(Button)`
-`;
+export const SubmitButton = styled(Button)``;
 
 export const Title = styled.Text`
   margin-top: 16px;
   font-size: 14px;
   line-height: 17px;
   color: #f2ebeb;
-  text-shadow: 0px 4px 4px rgba(255, 255, 255, 0.6);
 `;
